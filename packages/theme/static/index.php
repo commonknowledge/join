0c8ea7970c91640d2ea026d7498f21fac871108b
Alex Andrews <alex@alexandrews.info>
2020-10-15 08:18:02 +0100

    Add in the loop to display page content

--- packages/theme/static/index.php
+++ packages/theme/static/index.php
@@ -14,7 +14,13 @@
 	<?php get_template_part('template-parts/page-header') ?>
 
 	<main class="container mt-4 mb-5">
-		<?php the_content(); ?>
+		<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+		?>
+			<?php the_content(); ?>
+		<?php endwhile; endif; ?>
 	</main>
 
 	<?php get_template_part('template-parts/page-footer') ?>
