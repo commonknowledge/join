dffd05716b2037146eaf6885de905bb3d3aa7693
Alex Andrews <alex@alexandrews.info>
2020-10-15 12:52:33 +0100

    Further neatening

--- packages/theme/static/template-parts/page-footer.php
+++ packages/theme/static/template-parts/page-footer.php
@@ -1,26 +1,26 @@
-<footer class="container-fluid page-footer">
+<footer class="container-fluid page-footer py-4">
 	<div class="container">
-	<div class="row">
-		<div class="col-lg-6">
-		<div>
-			<ul class="list-unstyled page-footer-link">
-			<li><a href="#">Terms and conditions</a></li>
-			<li><a href="#">Privacy policy</a></li>
-			<li><a href="#">Contact us</a></li>
-			<li><a href="#">Send us feedback</a></li>
-			</u>
+		<div class="row">
+			<div class="col-lg-6">
+			<div>
+				<ul class="list-unstyled page-footer-link">
+				<li><a href="#">Terms and conditions</a></li>
+				<li><a href="#">Privacy policy</a></li>
+				<li><a href="#">Contact us</a></li>
+				<li><a href="#">Send us feedback</a></li>
+				</u>
+			</div>
+			<div class="text-xxs">
+				Published and promoted by Judy Maciejowska on behalf of the Green Party, both at The Biscuit Factory, Unit 215 J Block, 100 Clements Road, London, SE16 4DG.
+			</div>
+			<div class="text-xxs pt-4">
+				© All rights reserved. The Green Party 2006 - 2019
+			</div>
+			</div>
+			<div class="col"></div>
+			<div class="col col-lg-2">
+			<img alt="Logo" class="page-footer-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-footer.png">
+			</div>
 		</div>
-		<div class="text-xxs">
-			Published and promoted by Judy Maciejowska on behalf of the Green Party, both at The Biscuit Factory, Unit 215 J Block, 100 Clements Road, London, SE16 4DG.
-		</div>
-		<div class="text-xxs">
-			© All rights reserved. The Green Party 2006 - 2019
-		</div>
-		</div>
-		<div class="col"></div>
-		<div class="col col-lg-2">
-		<img alt="Logo" class="page-footer-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-footer.png">
-		</div>
-	</div>
 	</div>
 </footer>
