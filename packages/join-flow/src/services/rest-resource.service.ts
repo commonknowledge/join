683acae5f463224057ab98fe246e9cccd9e6268f
Alex Andrews <alex@alexandrews.info>
2020-10-16 07:47:58 +0100

    Add support for looking up plan from membership

--- packages/join-flow/src/services/rest-resource.service.ts
+++ packages/join-flow/src/services/rest-resource.service.ts
@@ -1,7 +1,19 @@
+const membershipToPlan = {
+	standard: 'membership_annual_individual_waged',
+	international: 'membership_annual_international',
+	lowWaged: 'membership_annual_individual_low_waged',
+	unwaged: 'membership_annual_unwaged'
+}
 
 export const usePostResource = <Params, Result = {}>(resource: string) => {
 	return async (data: Params): Promise<Result> => {
 		const endpoint = '/join/v1' + resource
+		
+		// @ts-ignore
+		data.plan = membershipToPlan[data.membership];
+		
+		console.log(data);
+
 		const res = await fetch('/?rest_route=' + endpoint, {
 			method: 'POST',
 			headers: {
