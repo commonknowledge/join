ff76764daa3bcd588af49ea0e5f3821fb516b7bb
Alex Andrews <alex@alexandrews.info>
2020-10-16 06:55:15 +0100

    Handle build in Yarn, do one thing in this script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,6 @@ function clean() {
 theme_dest=$content_dest/themes/greens
 plugin_dest=$content_dest/plugins/join-form
 
-yarn build
-
 clean $theme_dest
 deploy_all packages/theme/dist $theme_dest
 
