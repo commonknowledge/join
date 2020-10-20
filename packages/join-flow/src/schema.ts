e3b87f6ff21de250d9683f47e4196c81b53fc0bb
Alex Andrews <alex@alexandrews.info>
2020-10-20 07:40:20 +0100

    Prettier document, add phone number validation

--- packages/join-flow/src/schema.ts
+++ packages/join-flow/src/schema.ts
@@ -1,163 +1,176 @@
 import { memoize } from "lodash-es";
 import { boolean, InferType, number, object, ObjectSchema, string } from "yup";
+import "yup-phone";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-const Prerequesites = object({
-	/** Email passed in via query param */
-	email: string().required(),
+declare module "yup" {
+  export interface StringSchema {
+    phone(
+      countryCode?: string,
+      strict?: boolean,
+      errorMessage?: string
+    ): StringSchema;
+  }
+}
 
-	/** Session token required by GoCardless. Generated client-side on load. */
-	sessionToken: string().required()
+const Prerequesites = object({
+  /** Email passed in via query param */
+  email: string().required(),
+
+  /** Session token required by GoCardless. Generated client-side on load. */
+  sessionToken: string().required()
 }).required();
 
 export const DetailsSchema = object({
-	firstName: string().required("First name is required"),
-	lastName: string().required("Second name is required"),
-	email: string()
-		.email("Your email address must be a valid email address")
-		.required("Email address is required"),
-	dobDay: number()
-		.typeError("The day of your birth must be a number")
-		.integer()
-		.max(
-			31,
-			"The day of your birth should be a number between 1 and 31, representing the days of the month"
-		)
-		.required(),
-	dobMonth: number()
-		.typeError("The month of your birth must be a number")
-		.integer()
-		.max(
-			12,
-			"The month of your birth should be only be a number between 1 and 12, representing the months of the year"
-		)
-		.required(),
-	dobYear: number()
-		.typeError("The year of your birth must be a number")
-		.integer()
-		.max(
-			9999,
-			"The year of your birth should be only be a maximum of four numbers, for example 1984"
-		)
-		.min(
-			2,
-			"The year of your birth should be only be a minimum of two numbers, for example 1984"
-		)
-		.required(),
-	addressLine1: string().required(),
-	addressLine2: string(),
-	addressCity: string().required(),
-	addressCounty: string().required(),
-	addressPostcode: string().required(),
-	addressCountry: string().required(),
-	phoneNumber: string().required(),
-	contactViaEmail: boolean(),
-	contactViaPhone: boolean()
+  firstName: string().required("First name is required"),
+  lastName: string().required("Second name is required"),
+  email: string()
+    .email("Your email address must be a valid email address")
+    .required("Email address is required"),
+  dobDay: number()
+    .typeError("The day of your birth must be a number")
+    .integer()
+    .max(
+      31,
+      "The day of your birth should be a number between 1 and 31, representing the days of the month"
+    )
+    .required(),
+  dobMonth: number()
+    .typeError("The month of your birth must be a number")
+    .integer()
+    .max(
+      12,
+      "The month of your birth should be only be a number between 1 and 12, representing the months of the year"
+    )
+    .required(),
+  dobYear: number()
+    .typeError("The year of your birth must be a number")
+    .integer()
+    .max(
+      9999,
+      "The year of your birth should be only be a maximum of four numbers, for example 1984"
+    )
+    .min(
+      2,
+      "The year of your birth should be only be a minimum of two numbers, for example 1984"
+    )
+    .required(),
+  addressLine1: string().required(),
+  addressLine2: string(),
+  addressCity: string().required(),
+  addressCounty: string().required(),
+  addressPostcode: string().required(),
+  addressCountry: string().required(),
+  phoneNumber: string()
+    .phone(undefined, false, "A valid phone number is required")
+    .required(),
+  contactViaEmail: boolean(),
+  contactViaPhone: boolean()
 }).required();
 
 const PlanSchema = object({
-	membership: string()
-		.oneOf(["standard", "lowWaged", "international", "unwaged"])
-		.required()
+  membership: string()
+    .oneOf(["standard", "lowWaged", "international", "unwaged"])
+    .required()
 }).required();
 
 export const renderPaymentPlan = ({ membership }: FormSchema) => {
-	if (membership === "standard") {
-		return "Standard Membership";
-	}
-	if (membership === "international") {
-		return "International Membership";
-	}
-	if (membership === "lowWaged") {
-		return "Low-Waged Membership";
-	}
-	if (membership === "unwaged") {
-		return "Unwaged or Student Membership";
-	}
+  if (membership === "standard") {
+    return "Standard Membership";
+  }
+  if (membership === "international") {
+    return "International Membership";
+  }
+  if (membership === "lowWaged") {
+    return "Low-Waged Membership";
+  }
+  if (membership === "unwaged") {
+    return "Unwaged or Student Membership";
+  }
 
-	return "None";
+  return "None";
 };
 
 const PaymentMethodSchema = object({
-	paymentMethod: string().oneOf(["directDebit", "creditCard"]).required()
+  paymentMethod: string().oneOf(["directDebit", "creditCard"]).required()
 }).required();
 
 export const renderPaymentMethod = ({ paymentMethod }: FormSchema) => {
-	if (paymentMethod === "creditCard") {
-		return "Credit Card";
-	}
-	if (paymentMethod === "directDebit") {
-		return "Monthly Direct Debit";
-	}
+  if (paymentMethod === "creditCard") {
+    return "Credit Card";
+  }
+  if (paymentMethod === "directDebit") {
+    return "Monthly Direct Debit";
+  }
 
-	return "None";
+  return "None";
 };
 
 const PaymentMethodDDSchema = object({
-	paymentMethod: string().equals(["directDebit"]).required(),
-	ddAccountHolderName: string().required(),
-	ddAccountNumber: string().required(),
-	ddSortCode: string().required(),
-	ddConfirmAccountHolder: boolean().equals([true]).required()
+  paymentMethod: string().equals(["directDebit"]).required(),
+  ddAccountHolderName: string().required(),
+  ddAccountNumber: string().required(),
+  ddSortCode: string().required(),
+  ddConfirmAccountHolder: boolean().equals([true]).required()
 }).required();
 
 const PaymentMethodCardSchema = object({
-	paymentMethod: string().equals(["creditCard"]).required(),
-	paymentToken: string().required()
+  paymentMethod: string().equals(["creditCard"]).required(),
+  paymentToken: string().required()
 }).required();
 
 const PaymentDetailsSchema = PaymentMethodDDSchema.concat(
-	PaymentMethodCardSchema
+  PaymentMethodCardSchema
 );
 
 export const FormSchema: ObjectSchema<FormSchema> = object()
-	.concat(Prerequesites)
-	.concat(DetailsSchema)
-	.concat(PlanSchema)
-	.concat(PaymentMethodSchema)
-	.concat(PaymentDetailsSchema)
-	.required();
+  .concat(Prerequesites)
+  .concat(DetailsSchema)
+  .concat(PlanSchema)
+  .concat(PaymentMethodSchema)
+  .concat(PaymentDetailsSchema)
+  .required();
 
 export type FormSchema = Partial<
-	InferType<typeof Prerequesites> &
-		InferType<typeof DetailsSchema> &
-		InferType<typeof PlanSchema> &
-		(
-			| InferType<typeof PaymentMethodDDSchema>
-			| InferType<typeof PaymentMethodCardSchema>
-		) &
-		InferType<typeof PaymentDetailsSchema>
+  InferType<typeof Prerequesites> &
+    InferType<typeof DetailsSchema> &
+    InferType<typeof PlanSchema> &
+    (
+      | InferType<typeof PaymentMethodDDSchema>
+      | InferType<typeof PaymentMethodCardSchema>
+    ) &
+    InferType<typeof PaymentDetailsSchema>
 >;
 
 export const getTestDataIfEnabled = (): FormSchema => {
-	if (process.env.REACT_APP_USE_TEST_DATA) {
-		console.log(
-			"REACT_APP_USE_TEST_DATA environment variable set. Using test data."
-		);
-		return {
-			email: "me@example.com",
-			addressCity: "Oxford",
-			addressCountry: "GB",
-			addressCounty: "Oxfordshire",
-			addressLine1: "54 Abingdon Road",
-			addressLine2: "",
-			addressPostcode: "OX14PE",
-			dobDay: 5,
-			dobMonth: 12,
-			dobYear: 1980,
-			firstName: "Test",
-			lastName: "Person",
-			membership: "standard",
-			paymentMethod: "directDebit",
-			ddAccountNumber: " 55779911",
-			ddSortCode: "200000",
-			phoneNumber: "02036919400",
-			contactViaEmail: true,
-			contactViaPhone: true
-		};
-	} else {
-		return {};
-	}
+  if (process.env.REACT_APP_USE_TEST_DATA) {
+    console.log(
+      "REACT_APP_USE_TEST_DATA environment variable set. Using test data."
+    );
+    return {
+      email: "me@example.com",
+      addressCity: "Oxford",
+      addressCountry: "GB",
+      addressCounty: "Oxfordshire",
+      addressLine1: "54 Abingdon Road",
+      addressLine2: "",
+      addressPostcode: "OX14PE",
+      dobDay: 5,
+      dobMonth: 12,
+      dobYear: 1980,
+      firstName: "Test",
+      lastName: "Person",
+      membership: "standard",
+      paymentMethod: "directDebit",
+      ddAccountNumber: " 55779911",
+      ddSortCode: "200000",
+      phoneNumber: "02036919400",
+      contactViaEmail: true,
+      contactViaPhone: true
+    };
+  } else {
+    return {};
+  }
 };
 
 export const validate = memoize((schema: ObjectSchema) => yupResolver(schema));
