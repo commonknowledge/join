ec9c040131f8f313816a43b50a8e047801451c9e
Alex Andrews <alex@alexandrews.info>
2020-10-23 12:07:19 +0100

    Replace logo with correct logo as SVG

--- packages/theme/static/template-parts/full-page-header.php
+++ packages/theme/static/template-parts/full-page-header.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
 	<div class="container">
-		<img alt="Green Party" class="page-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+		<img alt="Green Party" class="page-logo" src="<?php echo get_template_directory_uri(); ?>/img/green-party-logo.svg">
 		<div class="navbar-nav d-flex flex-row align-items-center text-bebas-neue">
 			<div class="nav-item mr-3 text-uppercase"><a class="text-black" href="#">Covid-19 Response</a></div>
 			<div class="nav-item mr-3 text-uppercase"><a class="text-black" href="#">Volunteer</a></div>
