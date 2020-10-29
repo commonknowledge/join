be3552d1d7d1f82f1ba76ebfd6cd6981c3afc4dd
Alex Andrews <alex@alexandrews.info>
2020-10-29 12:42:54 +0000

    Move chevron to a component

--- packages/join-flow/src/components/atoms.tsx
+++ packages/join-flow/src/components/atoms.tsx
@@ -7,25 +7,29 @@ interface ContinueButtonProps {
   text?: string;
 }
 
+const ChevronSVG = (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    className="bi bi-chevron-right"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+    />
+  </svg>
+);
+
 export const ContinueButton: FC<ContinueButtonProps> = ({ text }) => (
   <Button
     className="form-section-addon d-flex align-items-center"
     type="submit"
   >
     {text || "Continue"}
-    <svg
-      width="1em"
-      height="1em"
-      viewBox="0 0 16 16"
-      className="bi bi-chevron-right"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-      />
-    </svg>
+    {ChevronSVG}
   </Button>
 );
 
