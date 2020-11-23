6b4059f358f027fe97ac04bf161329bf82fec25e
Alex Andrews <alex@alexandrews.info>
2020-11-23 17:55:17 +0000

    Remove space

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,7 +9,7 @@ echo "done"
 
 echo -n "Creating plugin zip file..."
 cp -r packages/join-block/ dist/join-block
-cd dist/join-block 
+cd dist/join-block
 rm .env
 rm -rf node_modules
 zip -r -q green-party-join-plugin ./
