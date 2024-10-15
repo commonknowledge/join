0b2ab6950e8ca9371e5c356c2dfb60d92748b906
Joaquim d'Souza <joaquimds@gmail.com>
2024-10-15 17:43:56 +0200

    feat: add action network support

--- packages/join-block/src/Services/MailchimpService.php
+++ packages/join-block/src/Services/MailchimpService.php
@@ -7,11 +7,13 @@ use CommonKnowledge\JoinBlock\Settings;
 
 class MailchimpService
 {
-    public static function signup($email)
+    public static function signup($data)
     {
         global $joinBlockLog;
 
-        $joinBlockLog->info("Adding $email to Mailchimp");
+        $email = $data['email'];
+
+        $joinBlockLog->info("Adding {$data['email']} to Mailchimp");
 
         $mailchimp_api_key = Settings::get("MAILCHIMP_API_KEY");
         $mailchimp_audience_id = Settings::get("MAILCHIMP_AUDIENCE_ID");
@@ -24,10 +26,29 @@ class MailchimpService
             'server' => $server
         ]);
 
+        if ($data['isUpdateFlow']) {
+            $mergeFields = [];
+        } else {
+            $mergeFields = [
+                "FNAME" => $data['firstName'],
+                "LNAME" => $data['lastName'],
+                "PHONE" => $data['phoneNumber'],
+                "ADDRESS" => [
+                    "addr1" => $data["addressLine1"],
+                    "addr2" => $data["addressLine2"],
+                    "city" => $data["addressCity"],
+                    "state" => "",
+                    "zip" => $data["addressPostcode"],
+                    "country" => $data["addressCountry"]
+                ]
+            ];
+        }
+
         try {
             $mailchimp->lists->addListMember($mailchimp_audience_id, [
                 "email_address" => $email,
                 "status" => "subscribed",
+                "merge_fields" => $mergeFields
             ]);
             $joinBlockLog->info("$email added to Mailchimp");
         } catch (\GuzzleHttp\Exception\ClientException $e) {
