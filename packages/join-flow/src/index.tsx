51d4efae59b0b7ff92005bfb433d8256651af478
Joaquim d'Souza <joaquimds@gmail.com>
2025-11-05 18:57:44 +0100

    chore: bump version

--- packages/join-flow/src/index.tsx
+++ packages/join-flow/src/index.tsx
@@ -24,7 +24,7 @@ const init = () => {
   const sentryDsn = getEnvStr("SENTRY_DSN")
   Sentry.init({
     dsn: sentryDsn,
-    release: "1.3.1"
+    release: "1.3.2"
   });
 
   if (getEnv('USE_CHARGEBEE')) {
