307248a7845b497ed55d74194f590311b7c11a60
Joaquim d'Souza <joaquimds@gmail.com>
2026-05-07 13:06:49 +0200

    Bump version to 1.4.12

--- packages/join-flow/src/index.tsx
+++ packages/join-flow/src/index.tsx
@@ -24,7 +24,7 @@ const init = () => {
   const sentryDsn = getEnvStr("SENTRY_DSN")
   Sentry.init({
     dsn: sentryDsn,
-    release: "1.4.11"
+    release: "1.4.12"
   });
 
   if (getEnv('USE_CHARGEBEE')) {
