ed69df36344ad2a7403965b6743a3736efbdd7a5
Alex Worrad-Andrews <alex@alexandrews.info>
2026-09-16 13:08:09 +0100

    Bump version to 1.4.38

--- packages/join-flow/src/index.tsx
+++ packages/join-flow/src/index.tsx
@@ -24,7 +24,7 @@ const init = () => {
   const sentryDsn = getEnvStr("SENTRY_DSN")
   Sentry.init({
     dsn: sentryDsn,
-    release: "1.4.37"
+    release: "1.4.38"
   });
 
   if (getEnv('USE_CHARGEBEE')) {
